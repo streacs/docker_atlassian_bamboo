2fa8507c95b67cb4f377987b5743f493036722f6
Oliver Wolf <root@streacs.com>
2018-01-02 22:37:45 +0100

    disabled notifications

--- bamboo-specs/src/main/java/com/streacs/java/PlanSpec.java
+++ bamboo-specs/src/main/java/com/streacs/java/PlanSpec.java
@@ -67,6 +67,7 @@ public class PlanSpec {
             project(),
             "STREACS Atlassian Bamboo", "CF54AB")
             .enabled(true)
+            .noNotifications()
             .linkedRepositories("DCK - STREACS Atlassian Bamboo (master)")
             .planBranchManagement(new PlanBranchManagement()
                 .createForVcsBranchMatching("^feature/.*|^release/.*|^develop")
